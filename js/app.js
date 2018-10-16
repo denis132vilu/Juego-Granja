eab062e8b3b0b430e1c5fc19d07e2024160cbd8c
Denis Villagran <denisvillagran99@gmail.com>
2018-10-16 15:33:04 -0600

    Dibujando objetos en el tablero

--- js/app.js
+++ js/app.js
@@ -6,13 +6,40 @@ var fondo = {
     imagen: Image
 };
 
+var vaca = {
+    url: './imagenes/vaca.png',
+    imagen: Image
+};
+
+var cerdo = {
+    url: './imagenes/cerdo.png',
+    imagen: Image
+};
+
 fondo.imagen = new Image();
+vaca.imagen = new Image();
+cerdo.imagen = new Image();
 fondo.imagen.src = fondo.url;
+vaca.imagen.src = vaca.url;
+cerdo.imagen.src = cerdo.url;
 
 fondo.imagen.addEventListener("load", function() {//Agregar evento de cuando se carga la pagina y se le asigna una funcion
-    dibujar();
+    lapiz.drawImage(fondo.imagen,0,0);
+    // dibujar();
+});
+
+vaca.imagen.addEventListener("load", function(){
+    lapiz.drawImage(vaca.imagen,150,100);
+    // dibujar();
+});
+
+cerdo.imagen.addEventListener("load", function(){
+    lapiz.drawImage(cerdo.imagen,200,300);
+    // dibujar();
 });
 
 function dibujar() {
     lapiz.drawImage(fondo.imagen,0,0);
+    lapiz.drawImage(vaca.imagen,150,100);
+    lapiz.drawImage(cerdo.imagen,200,120);    
 };
