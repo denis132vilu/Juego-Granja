e458a9d171131ccda6ddee08f526d1aba054b4ed
Denis Villagran <denisvillagran99@gmail.com>
2018-10-17 08:43:10 -0600

    Mostrando todas las imagenes en la pagina

--- js/app.js
+++ js/app.js
@@ -3,43 +3,71 @@ var lapiz = canvas.getContext('2d');
 
 var fondo = {
     url: './imagenes/tile.png',
-    imagen: Image
+    imagen: Image,
+    cargaOK: false
 };
 
 var vaca = {
     url: './imagenes/vaca.png',
-    imagen: Image
+    imagen: Image,
+    cargaOK: false
 };
 
 var cerdo = {
     url: './imagenes/cerdo.png',
-    imagen: Image
+    imagen: Image,
+    cargaOK: false
+};
+
+var cuchillo = {
+    url: './imagenes/cuchillo.png',
+    imagen: Image,
+    cargaOK: false
 };
 
 fondo.imagen = new Image();
 vaca.imagen = new Image();
 cerdo.imagen = new Image();
+cuchillo.imagen = new Image();
 fondo.imagen.src = fondo.url;
 vaca.imagen.src = vaca.url;
 cerdo.imagen.src = cerdo.url;
+cuchillo.imagen.src = cuchillo.url;
 
 fondo.imagen.addEventListener("load", function() {//Agregar evento de cuando se carga la pagina y se le asigna una funcion
-    lapiz.drawImage(fondo.imagen,0,0);
-    // dibujar();
+    fondo.cargaOK = true;
+    dibujar();
+    // lapiz.drawImage(fondo.imagen,0,0);
 });
 
 vaca.imagen.addEventListener("load", function(){
-    lapiz.drawImage(vaca.imagen,150,100);
-    // dibujar();
+    vaca.cargaOK = true;
+    dibujar();
+    // lapiz.drawImage(vaca.imagen,150,100);
 });
 
 cerdo.imagen.addEventListener("load", function(){
-    lapiz.drawImage(cerdo.imagen,200,300);
-    // dibujar();
+    cerdo.cargaOK = true;
+    dibujar();
+    // lapiz.drawImage(cerdo.imagen,200,300);
+});
+
+cuchillo.imagen.addEventListener("load", function() {
+    cuchillo.cargaOK = true;
+    dibujar();
 });
 
 function dibujar() {
-    lapiz.drawImage(fondo.imagen,0,0);
-    lapiz.drawImage(vaca.imagen,150,100);
-    lapiz.drawImage(cerdo.imagen,200,120);    
+    if (fondo.cargaOK) {
+        lapiz.drawImage(fondo.imagen,0,0);
+    };
+    if (vaca.cargaOK) {
+        lapiz.drawImage(vaca.imagen,150,100);
+    };
+    if (cerdo.cargaOK) {
+        lapiz.drawImage(cerdo.imagen,200,200);
+    };
+    if (cuchillo.cargaOK) {
+        lapiz.drawImage(cuchillo.imagen, 300,200);
+    }
 };
